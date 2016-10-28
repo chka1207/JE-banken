e04e68f11efa3b597733bf5ae52efe2d028f9946
Christoffer Karlsson <chka1207@student.miun.se>
2016-10-28 13:20:22 +0200

    Fyllt på i xml-filerna, dock inte klar. Skapat class för frågor

---
+++ JE-Bank/JE-Bank/Fråga.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JE_Bank
+{
+    public class Fråga
+    {
+        public string fråga { get; set; }
+        public string provdel { get; set; }
+        public string svar1 { get; set; }
+        public string svar2 { get; set; }
+        public string svar3 { get; set; }
+        public string svar4 { get; set; }
+    }
+}
