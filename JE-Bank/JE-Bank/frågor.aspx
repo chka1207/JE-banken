ceed4c44c3d369922774ff6559b8be2ac002098e
Nicklas Persson <gorlingy@hotmail.com>
2016-11-02 15:05:21 +0100

    Knappar

--- JE-Bank/JE-Bank/frågor.aspx
+++ JE-Bank/JE-Bank/frågor.aspx
@@ -1,6 +1,7 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="frågor.aspx.cs" Inherits="JE_Bank.frågor" %>
+﻿<%@ Page Title=""  Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="frågor.aspx.cs" Inherits="JE_Bank.frågor" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div id="test"></div>
+    <div id="test" runat="server"></div>
+    
 </asp:Content>
--- JE-Bank/JE-Bank/frågor.aspx.cs
+++ JE-Bank/JE-Bank/frågor.aspx.cs
@@ -4,19 +4,100 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
 
 namespace JE_Bank
 {
     public partial class frågor : System.Web.UI.Page
     {
+        List<Fråga> lista = new List<Fråga>();
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            test1();
+            laddaFragor();
         }
 
-        public void Fragor()
+        public void laddaFragor()
         {
+            string fråga;
+            HtmlGenericControl div2 = new HtmlGenericControl("div");
+            div2.Attributes.Add("id", "div2");
 
+            for (int i = 0; i < lista.Count; i++)
+            {
+                HtmlGenericControl div = new HtmlGenericControl("div");
+                HtmlInputRadioButton rd1 = new HtmlInputRadioButton();
+                HtmlGenericControl rd11 = new HtmlGenericControl("span");
+                HtmlInputRadioButton rd2 = new HtmlInputRadioButton();
+                HtmlGenericControl rd22 = new HtmlGenericControl("span");
+                HtmlInputRadioButton rd3 = new HtmlInputRadioButton();
+                HtmlGenericControl rd33 = new HtmlGenericControl("span");
+                HtmlInputRadioButton rd4 = new HtmlInputRadioButton();
+                HtmlGenericControl rd44 = new HtmlGenericControl("span");
+                div.Attributes.Add("id", lista[i].fråga  );
+                rd1.Attributes.Add("id", "rd" + lista[i].svar1 + "");
+                rd1.Attributes.Add("value", lista[i].svar1);
+                rd2.Attributes.Add("id", "rd'" + lista[i].svar2 + "'");
+                rd3.Attributes.Add("id", "rd'" + lista[i].svar3 + "'");
+                rd4.Attributes.Add("id", "rd'" + lista[i].svar4 + "'");
+                rd1.Attributes.Add("name", lista[i].fråga);
+                rd2.Attributes.Add("name", lista[i].fråga);
+                rd3.Attributes.Add("name", lista[i].fråga);
+                rd4.Attributes.Add("name", lista[i].fråga);
+                fråga = lista[i].provdel + "<br />" + lista[i].fråga + "<br />";
+                //  "<input type='radio' id='" + lista[i].svar2 + "'name='radiobutton' value='" + lista[i].svar2 +"'>" + lista[i].svar2 + "<br />" + 
+                //  "<input type='radio' id='" + lista[i].svar3 + "'name='radiobutton' value='" + lista[i].svar3 +"'>" + lista[i].svar3 + "<br />" + 
+                //  "<input type='radio' id='" + lista[i].svar4 + "'name='radiobutton' value='" + lista[i].svar4 +"'>" + lista[i].svar4 + "<br />" + 
+                //  "<br /> </form>";
+
+                rd11.InnerText = lista[i].svar1;
+                rd22.InnerText = lista[i].svar2;
+                rd33.InnerText = lista[i].svar3;
+                rd44.InnerText = lista[i].svar4;
+                div.InnerHtml = fråga;
+               
+                div.Controls.Add(rd1);
+                div.Controls.Add(rd11);
+                div.Controls.Add(rd2);
+                div.Controls.Add(rd22);
+                div.Controls.Add(rd3);
+                div.Controls.Add(rd33);
+                div.Controls.Add(rd4);
+                div.Controls.Add(rd44);
+                test.Controls.Add(div);
+            }
+        }
+        public void check()
+        {
+          
+            
+        }
+    public void test1()
+        {
+            Fråga f = new Fråga();
+            f.provdel = "Etik";
+            f.fråga = "vad är grus?";
+            f.svar1 = "jesus";
+            f.svar2 = "kul";
+            f.svar3 = "hajk";
+            f.svar4 = "baloba";
+            Fråga f1 = new Fråga();
+            lista.Add(f);
+            f1.provdel = "Etik2";
+            f1.fråga = "vad är grus?2";
+            f1.svar1 = "jesus2";
+            f1.svar2 = "kul2";
+            f1.svar3 = "hajk2";
+            f1.svar4 = "baloba2";
+            lista.Add(f1);
+            Fråga f2 = new Fråga();
+            f2.provdel = "Etik3";
+            f2.fråga = "vad är grus?3";
+            f2.svar1 = "jesus2";
+            f2.svar2 = "kul2";
+            f2.svar3 = "hajk2";
+            f2.svar4 = "baloba2";
+            lista.Add(f2);
         }
     }
 }
