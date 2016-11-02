2a8468f3ed51a67428bc5ac7b56956abab2281b4
Christoffer Karlsson <chka1207@student.miun.se>
2016-11-02 13:56:09 +0100

    Bilder laddas som objekt tillsammas med rätt fråga och provdel, skrivs dock ut i fel format.

--- JE-Bank/JE-Bank/kunskap.aspx.cs
+++ JE-Bank/JE-Bank/kunskap.aspx.cs
@@ -66,7 +66,16 @@ namespace JE_Bank
             foreach (Fråga f in frågelista)
             {
                 HtmlGenericControl div = new HtmlGenericControl("div");
-                div.InnerHtml = f.provdel +" " + f.fråga;
+                if (f.bild != "")
+                {
+                    
+                    div.InnerHtml = f.provdel + " " + f.fråga + " " + f.bild;
+                }
+                else
+                {
+                    div.InnerHtml = f.provdel + " " + f.fråga;
+                }
+                
                 test.Controls.Add(div);
             }
         }
@@ -79,19 +88,18 @@ namespace JE_Bank
             string path = Server.MapPath(@"xml\kunskap.xml");
             XmlDocument doc = new XmlDocument();
             doc.Load(path);
-
-            XmlNodeList provdel = doc.SelectNodes("/kunskapsprov/provdel");
-            XmlNodeList frågan = doc.SelectNodes("/kunskapsprov/provdel/fråga");
+            XmlNodeList provdel = doc.SelectNodes("/kunskapsprov/provdel/fråga");
+            
 
             foreach (XmlNode node in provdel)
             {
                 Fråga f = new Fråga();
-                f.provdel = node["namn"].InnerText;
-                foreach (XmlNode node2 in frågan)
-                {
-                    
-                    f.fråga = node2["text"].InnerText;
-                }
+
+                f.fråga = node["text"].InnerText;
+                f.provdel = node.ParentNode["namn"].InnerText;
+                f.bild = node["text"].Attributes["bild"].InnerText;
+                                
+
 
                 x.Add(f);
             }
