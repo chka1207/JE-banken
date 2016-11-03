3fc2fe431ccafb5af59933754350f4e8df9fb578
Nicklas Persson <gorlingy@hotmail.com>
2016-11-04 00:02:26 +0100

    Fixat uträkningen

--- JE-Bank/JE-Bank/resultat.aspx.cs
+++ JE-Bank/JE-Bank/resultat.aspx.cs
@@ -26,14 +26,15 @@ namespace JE_Bank
             if (Session["resul"] != null)
             { 
             string resultat1 = Session["Resul"].ToString();
-            int del1 = Convert.ToInt16(Session["del1"].ToString());
-            int del2 = Convert.ToInt16(Session["del2"].ToString());
-            int del3 = Convert.ToInt16(Session["del3"].ToString());
-            double del11;
-            double del21;
-            double del31;
-            double total;
-            int total1=del1 + del2 + del3;
+            double del1 = Convert.ToDouble(Session["del1"].ToString());
+            double del2 = Convert.ToDouble(Session["del2"].ToString());
+            double del3 = Convert.ToDouble(Session["del3"].ToString());
+           
+            double total = del1 + del2 + del3;
+                double total1;
+                double del11, del21, del31;
+
+
                 lbresultat1.Visible = true;
                 lbresultat2.Visible = true;
                 lbresultat3.Visible = true;
@@ -45,7 +46,7 @@ namespace JE_Bank
                     del11 = del1 / 5 * 100;
                     del21 = del2 / 5 * 100;
                     del31 = del3 / 5 * 100;
-                    total = total1 / 15 * 100;
+                    total1 = total / 15 * 100;
                     
                 }
             else
@@ -53,9 +54,9 @@ namespace JE_Bank
                     del11 = del1 / 10 * 100;
                     del21 = del2 / 5 * 100;
                     del31 = del3 / 10 * 100;
-                    total = total1 / 25 * 100;
+                    total1 = total / 25 * 100;
                 }
-            if (total> 69&& del11 > 59 && del21 >59 && del31 >59)
+            if (total1> 69&& del11 > 59 && del21 >59 && del31 >59)
                 {
                     lbresultat1.Text = "GODKÄND";
 
@@ -64,11 +65,15 @@ namespace JE_Bank
                 {
                     lbresultat1.Text = "INTE GODKÄND";
                 }
+                del11 = Math.Round(del11, 2);
+                del21 = Math.Round(del21, 2);
+                del31 = Math.Round(del21, 2);
+                total1 = Math.Round(total1, 2);
 
-            lbresultat2.Text = "Del 1: "+del1+ "st  " + del11 + "%";
+            lbresultat2.Text = "Del 1: "+del1 + "st  " + del11 + "%";
             lbresultat3.Text = "Del 2: " + del2 + "st  " + del21 + "%";
             lbresultat4.Text = "Del 3: " + del3 + "st  " + del31 + "%";
-            lbresultat5.Text = "Totalt: " + del1 + "st  " + total + "%";
+            lbresultat5.Text = "Totalt: " + total + "st  " + total1 + "%";
                 
             }
         }
